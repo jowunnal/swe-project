5b59e2fb419c23e0b49643ae0c2b0ea6d13d9655
jinho <whangwan@gmail.com>
2022-05-30 00:27:03 +0900

    알람기능구현완료

--- Swproject_API_TEST/app/src/main/java/org/jinhostudy/swproject/fragment/WaterFragment.kt
+++ Swproject_API_TEST/app/src/main/java/org/jinhostudy/swproject/fragment/WaterFragment.kt
@@ -8,10 +8,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
 import org.jinhostudy.swproject.databinding.WaterFragmentBinding
-import org.jinhostudy.swproject.viewmodel.CalendarViewModel
-import org.jinhostudy.swproject.viewmodel.CalendarViewModelFactory
-import org.jinhostudy.swproject.viewmodel.WaterViewModel
-import org.jinhostudy.swproject.viewmodel.WaterViewModelFactory
+import org.jinhostudy.swproject.viewmodel.*
 
 class WaterFragment : Fragment() {
     var _binding:WaterFragmentBinding ?= null
@@ -30,7 +27,7 @@ class WaterFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        calendarViewModel.day.observe(viewLifecycleOwner, Observer { s ->
+        calendarViewModel.day.observe(viewLifecycleOwner, Observer { s -> // 날짜선택 기능에따라 해당날짜의 물목표량 및 섭취량이 갱신
             waterViewModel.getDrinkGoal(s).observe(viewLifecycleOwner, Observer {
                 binding.progressBarWaterToday.max=it
                 binding.tvWaterGoal.text= it.toString()
@@ -42,19 +39,19 @@ class WaterFragment : Fragment() {
         })
 
         binding.tvWaterHeader.text="오늘 물 섭취량"
-        binding.buttonWaterAdd.setOnClickListener {
+        binding.buttonWaterAdd.setOnClickListener { //물 섭취량 혹은 목표량 증가버튼
             calendarViewModel.day.observe(viewLifecycleOwner, Observer {
                 waterViewModel.plusDrink(it)
             })
         }
-        binding.buttonWaterDec.setOnClickListener {
+        binding.buttonWaterDec.setOnClickListener { // 물 섭취량 혹은 목표량 감소버튼
             calendarViewModel.day.observe(viewLifecycleOwner, Observer {
                 waterViewModel.minusDrink(it)
             })
         }
 
 
-        binding.switch1.setOnCheckedChangeListener { p0, p1 ->
+        binding.switch1.setOnCheckedChangeListener { _, p1 -> // 물섭취량 혹은 물목표량 으로 스위치를 통해 변경
             when (p1) {
                 true -> {
                     binding.tvWaterHeader.text="오늘 물 목표량"
@@ -83,6 +80,20 @@ class WaterFragment : Fragment() {
                     }
                 }
             }
+        }
+        val alarmViewModel=AlarmViewModel(requireActivity().application)
+        binding.numberPickerAlarmTens.maxValue=9
+        binding.numberPickerAlarmTens.minValue=0
+        binding.numberPickerAlarmTens.value=0
+        binding.numberPickerAlarmUnits.maxValue=9
+        binding.numberPickerAlarmUnits.minValue=0
+        binding.numberPickerAlarmUnits.value=0
+        binding.buttonWaterAlarmAdd.setOnClickListener { //알람설정버튼
+            val min=binding.numberPickerAlarmTens.value.toString()+binding.numberPickerAlarmUnits.value.toString()
+            alarmViewModel.setAlarm(min.toInt())
+        }
+        binding.buttonWaterAlarmDelete.setOnClickListener {
+            alarmViewModel.clearAlarm()
         }
     }
 }
